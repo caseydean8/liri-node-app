bb579617d5b5a7c026e837e453b85285bc6fe9b0
Casey Carroll <caseydean8@gmail.com>
2019-07-27 21:48:26 -0400

    Added do-what-it-says functionality.

--- app.js
+++
@@ -1,3 +0,0 @@
-const inquirer = require('inquirer');
-
-var Spotify = require('node-spotify-api');
--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+var fs = require("fs");
+
 var keys = require("./keys.js");
 
 var Band = require("./band");
@@ -18,28 +20,53 @@ var command = process.argv[2];
 
 var searchTerm = process.argv.slice(3).join(" ");
 
-if (command === "concert-this") {
+var runIt = function() {
+    if (command === "concert-this") {
     console.log("Searching for concert");
     band.findBand(searchTerm)
-}
+    }
 
-if (command === "spotify-this-song") {
+    if (command === "spotify-this-song") {
     if (searchTerm){
-        console.log(searchTerm);
+    // console.log(searchTerm);
     console.log("Searching for song");
     song.findSong(searchTerm);
     }
     else {
-        song.findSong("the sign ace of base");
+    song.findSong("the sign ace of base");
     }
-}
+    }
 
-if (command === "movie") {
+    if (command === "movie") {
     if (searchTerm) {
     console.log("Searching for movie");
     movie.findMovie(searchTerm);
     }
     else {
-        movie.findMovie("mr nobody")
+    movie.findMovie("mr nobody")
     }
+    }
+
+    if (command === "do-what-it-says") {
+        runTextFile();
+        }
+        
+        function runTextFile(){
+            fs.readFile("random.txt", "UTF-8", function(error, data){
+                if (error){
+                    console.log(error);
+                }
+                    // console.log(data);
+                else {
+                    var textSplit = data.split(" ");
+                    command = textSplit[0];
+                    searchTerm = textSplit.slice(1).join(" ");
+                    runIt();
+                }
+            });
+            
+        };
 }
+
+runIt();
+
